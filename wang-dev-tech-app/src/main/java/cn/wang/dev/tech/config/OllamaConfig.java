1a977f626c5c74fd4a54d3b6944f8d82ff7ce862
@2501 90125561 <2501_90125561@gitcode.net>
2026-01-26 20:57:41 +0800

    feat: 第5节：Ollama DeepSeek流式应答页面对接

--- wang-dev-tech-app/src/main/java/cn/wang/dev/tech/config/OllamaConfig.java
+++ wang-dev-tech-app/src/main/java/cn/wang/dev/tech/config/OllamaConfig.java
@@ -1,10 +1,16 @@
 package cn.wang.dev.tech.config;
 
 import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.ollama.OllamaEmbeddingClient;
 import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * @author WangBigGun
@@ -24,4 +30,24 @@ public class OllamaConfig {
         return new OllamaChatClient(ollamaApi);
     }
 
+    @Bean
+    public TokenTextSplitter tokenTextSplitter() {
+        return new TokenTextSplitter();
+    }
+
+
+    @Bean
+    public SimpleVectorStore simpleVectorStore(OllamaApi ollamaApi) {
+        OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
+        embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
+        return new SimpleVectorStore(embeddingClient);
+
+    }
+
+    @Bean
+    public PgVectorStore vectorStore(OllamaApi ollamaApi, JdbcTemplate jdbcTemplate) {
+        OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
+        embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
+        return new PgVectorStore(jdbcTemplate, embeddingClient);
+    }
 }
